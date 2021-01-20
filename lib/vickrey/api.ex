786b2c2fbb73f53812bad6382b880d9cf79a316d
julienurraca@gmail.com <julienurraca@gmail.com>
2021-01-19 21:14:04 -0500

    generalize request format

--- lib/vickrey/api.ex
+++ lib/vickrey/api.ex
@@ -1,22 +1,29 @@
 defmodule Vickrey.API do
+  @moduledoc """
+  Main API utilities.
+  """
   alias Vickrey.API.Utils
 
-  def base_get_request() do
-    addr = Utils.get_node_address()
+  def base_request(addr) do
     headers = Utils.headers()
 
-    "http://#{addr}"
+    addr
     |> HTTPoison.get(headers)
     |> handle_response()
   end
 
+  def base_get_request() do
+    addr = Utils.get_node_address()
+
+    "http://#{addr}"
+    |> base_request()
+  end
+
   def base_get_request(opts) do
     addr = Utils.get_node_address()
-    headers = Utils.headers()
 
     "http://#{addr}/#{opts}"
-    |> HTTPoison.get(headers)
-    |> handle_response()
+    |> base_request()
   end
 
   def base_post_request(body) do
