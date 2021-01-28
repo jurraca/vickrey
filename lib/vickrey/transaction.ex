9a18256513d9e9cca9d298bb1b1612b96249e5ef
julienurraca@gmail.com <julienurraca@gmail.com>
2021-01-27 18:47:29 -0600

    fix the insert logic

--- lib/vickrey/transaction.ex
+++ lib/vickrey/transaction.ex
@@ -1,10 +1,26 @@
 defmodule Vickrey.Transaction do
     use Ecto.Schema
+    import Ecto.Changeset
 
     schema "transactions" do
         field :name, :string
         field :height, :integer
         field :action, :string
         field :value, :float
+    end
+
+    def changeset(tx, attrs \\ %{}) do
+        tx
+        |> cast(attrs, [:name, :height, :action, :value])
+    end
+
+    def insert(_, {:error, msg}), do: {:error, msg}
+
+    def insert(height, row = %{}) do
+        tx = Map.put_new(row, :height, height)
+
+        %Vickrey.Transaction{}
+        |> changeset(tx)
+        |> Vickrey.Repo.insert()
     end
 end
