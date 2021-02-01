8adb7d2f3b6a9cfd7c7e64305136eb0478a9357d
julienurraca@gmail.com <julienurraca@gmail.com>
2021-01-31 19:18:18 -0600

    initial query module, testing stuff out

---
+++ lib/vickrey/query.ex
@@ -0,0 +1,35 @@
+defmodule Vickrey.Query do
+    import Ecto.Query
+    alias Vickrey.{Repo, Transaction}
+
+    def get_name(name) when is_binary(name) do
+      Transaction
+        |> where([t], t.name == ^name)
+        |> order_by([t], [asc: t.height])
+        |> Repo.all()
+    end
+
+    def get_all_by_action(action) do
+      Transaction
+      |> where([t], t.action == ^action)
+      |> order_by([t], [asc: t.height])
+    end
+
+    def get_all_sold() do
+      get_all_by_action("CLOSED")
+    end
+
+    def get_all_open() do
+      get_all_by_action("OPEN")
+    end
+
+    def get_all_open_since(height) do
+      get_all_open()
+      |> where([t], t.height >= ^height)
+    end
+
+    def get_name_returns(name) do
+      get_name(name)
+      |> Enum.filter(fn %Vickrey.Transaction{action: action} -> action in ["CLOSED", "REGISTER"] end)
+    end
+end
