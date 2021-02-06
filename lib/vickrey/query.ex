0d74f32f7d8513055c627cb915cc00908bf74a02
julienurraca@gmail.com <julienurraca@gmail.com>
2021-02-06 12:16:51 -0600

    remove renamed file; compose queries more cleanly

--- lib/vickrey/query.ex
+++ lib/vickrey/query.ex
@@ -1,36 +1,34 @@
 defmodule Vickrey.Query do
     import Ecto.Query
-    alias Vickrey.{Repo, Transaction}
+    alias Vickrey.Transaction
 
     def get_name(name) when is_binary(name) do
       Transaction
         |> where([t], t.name == ^name)
         |> order_by([t], [asc: t.height])
-        |> Repo.all()
     end
 
     def get_name_closed(name) do
       name
       |> get_name()
-      |> Enum.filter(fn %Vickrey.Transaction{action: action} -> action in ["FINALIZE", "REGISTER"] end)
+      |> where([t], t.action in ["FINALIZE", "REGISTER"])
     end
 
-    def get_all_by_action(action) do
+    def get_by_action(action)do
       Transaction
-      |> where([t], t.action == ^action)
+      |> where([t], t.action in ^action)
       |> order_by([t], [asc: t.height])
     end
 
-    def get_all_sold() do
-      get_all_by_action("CLOSED")
-    end
-
-    def get_all_open() do
-      get_all_by_action("OPEN")
-    end
-
-    def get_all_open_since(height) do
-      get_all_open()
+    def get_by_action_since(action, height) do
+      action
+      |> get_by_action()
       |> where([t], t.height >= ^height)
     end
+
+    def get_sold(), do: get_by_action("REGISTER")
+
+    def get_opens(), do: get_by_action("OPEN")
+
+    def get_bids(), do: get_by_action("BID")
 end
