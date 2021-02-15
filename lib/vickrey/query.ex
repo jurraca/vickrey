0f01ba73f5d52753093a60a6dcfde237b747696e
julienurraca@gmail.com <julienurraca@gmail.com>
2021-02-15 13:41:06 -0600

    adjust name action queries

--- lib/vickrey/query.ex
+++ lib/vickrey/query.ex
@@ -1,6 +1,6 @@
 defmodule Vickrey.Query do
     import Ecto.Query
-    alias Vickrey.{Transaction, Repo}
+    alias Vickrey.{Transaction, Namebase.Record, Repo}
 
     def max_height(), do: Transaction |> select([t], max(t.height))
 
@@ -8,13 +8,18 @@ defmodule Vickrey.Query do
       Transaction
         |> where([t], t.name == ^name)
         |> order_by([t], [asc: t.height])
-        |> Repo.all()
     end
 
     def get_name_closed(name) do
       name
       |> get_name_txs()
-      |> where([t], t.action in ["FINALIZE", "REGISTER"])
+      |> where([t], t.action == ["REGISTER", "FINALIZE", "TRANSFER"])
+    end
+
+    def get_name_transferred(name) do
+      name
+      |> get_name_txs()
+      |> where([t], t.action in ["FINALIZE", "TRANSFER"])
     end
 
     def get_by_action(action)do
@@ -33,7 +38,16 @@ defmodule Vickrey.Query do
       |> where([t], t.height >= ^height)
     end
 
-    def get_sold(), do: get_by_action("REGISTER")
+    def with_sold(query) do
+      query |> join(:left, [t], s in Record, on: s.name == t.name)
+    end
+
+    def get_sold(name) when is_binary(name) do
+      Record
+      |> where([r], r.name == ^name)
+    end
+
+    def get_auction_closes(), do: get_by_action("REGISTER")
 
     def get_opens(), do: get_by_action("OPEN")
 
